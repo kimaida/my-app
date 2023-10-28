4b75f573448447f8f0cdcdeccf7755cf62f85ddb
imaida <Imaida.Kazunari@cti.co.jp>
2023-10-28 10:11:23 +0900

    Revert "frontend"
    This reverts commit e059e8bb8155b4241b494f91e2d658ba71b71842.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProducsModule } from './products/products.module';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  // { path: 'detail', component: ProductDetailComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-    ProducsModule
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
